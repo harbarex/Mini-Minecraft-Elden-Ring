4be5ca37ed9e9c5ed5cda936d6a691af26b1f551
MCChang macbook <meng.chuan.chang@gmail.com>
2022-11-21 18:53:19 -0500

    fix issue in input variable
    add fs_Pos in overlay.vert.glsl

--- assignment_package/glsl/post/overlay.vert.glsl
+++ assignment_package/glsl/post/overlay.vert.glsl
@@ -6,6 +6,7 @@ in vec4 vs_Pos;
 in vec4 vs_Col;
 in vec2 vs_UV;
 
+out vec4 fs_Pos;
 out vec4 fs_Col;
 out vec2 fs_UV;
 
@@ -13,5 +14,6 @@ void main()
 {
     fs_Col      = vs_Col;
     fs_UV       = vs_UV;
+    fs_Pos      = vs_Pos;
     gl_Position = vs_Pos;
 }
